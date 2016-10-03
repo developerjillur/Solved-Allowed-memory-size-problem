270afceff3da52bf0f9eafad0575a891bbb12036
Jillur Rahman <developerjillur@gmail.com>
2016-10-03 08:57:01 +0600

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+
+
+// put this code above "define('DB_NAME', 'DB Name');"
+
+ini_set('memory_limit', '524M');
+ini_set('max_execution_time', 360);
+ini_set('max_input_time', 120);
+
+define('WP_MEMORY_LIMIT', '120M');
+
+
